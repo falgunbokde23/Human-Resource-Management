8f5e113adcea21e0f221c8c40b3d54431407ce9b
Falgun Bokde <101393483+falgunbokde23@users.noreply.github.com>
2023-01-17 00:09:07 +0530

    Update acceptLeavesUsecase.java
    update

--- usecases/acceptLeavesUsecase.java
+++ usecases/acceptLeavesUsecase.java
@@ -9,16 +9,6 @@ public class acceptLeavesUsecase {
 	
 	public static void main(String[] args) {
 		
-//			Scanner sc = new Scanner(System.in);
-//			
-//			System.out.println("Enter Employee Id  : ");
-//			int empid=sc.nextInt();
-//			
-//			EmployeeDao dao = new EmployeeDaoImpl();
-//			
-//			String result = dao.acceptLeaves(empid);
-//			
-//			System.out.println(result);
 	}
 
 	
