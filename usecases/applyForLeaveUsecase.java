73acb882aff5fe6fc9ad253fe21f3764fc9dc5a5
Falgun Bokde <101393483+falgunbokde23@users.noreply.github.com>
2023-01-18 00:09:14 +0530

    Update applyForLeaveUsecase.java
    deleted extra unwanted code

--- usecases/applyForLeaveUsecase.java
+++ usecases/applyForLeaveUsecase.java
@@ -6,24 +6,6 @@ import com.masai.dao.EmployeeDaoImpl;
 
 public class applyForLeaveUsecase {
 	
-	public static void main(String[] args) {
-		
-		
-//		Scanner sc =new Scanner(System.in);
-//		
-//		System.out.println("Enter Employee ID :");
-//		int empid=sc.nextInt();
-//		
-//		System.out.println("Wants to Apply For Leave then Type Applied");
-//		String app=sc.next();
-//		
-//	    EmployeeDao dao=new EmployeeDaoImpl();
-//		
-//	    String result= dao.applyLeave(empid, app);
-//		
-//	    System.out.println(result);
-	    
-	}
 	
 	public static void applyLeaves() {
 	   
