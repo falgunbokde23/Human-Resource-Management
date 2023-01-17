a735543322778f6a37eb2e5f5b385384e31d4374
Falgun Bokde <101393483+falgunbokde23@users.noreply.github.com>
2023-01-18 00:10:14 +0530

    Update getEmployeeById.java
    deleted extra unwanted code

--- usecases/getEmployeeById.java
+++ usecases/getEmployeeById.java
@@ -8,30 +8,8 @@ import com.masai.dao.EmployeeDaoImpl;
 
 public class getEmployeeById { 
 	
-	public static void main(String[] args) {
-		
-		
-//		Scanner sc = new Scanner(System.in);
-//		
-//		System.out.println("Enter Employee Id : ");
-//		int eid=sc.nextInt();
-//		
-//		
-//		EmployeeDao dao= new EmployeeDaoImpl();
-//		
-//		
-//		try {
-//			
-//			Employee employee = dao.getEmployeeById(eid);
-//			System.out.println(employee);
-//			
-//			
-//			
-//		} catch (EmployeeException se) {
-//			System.out.println(se.getMessage());
-//		}
-//	
-	}
+	
+	
 	
 	
 	public static void viewEmployById() {
