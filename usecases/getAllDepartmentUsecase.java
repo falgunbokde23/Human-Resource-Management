f3e8ad26645d6b94382fb324960260a4cb0201b0
Falgun Bokde <101393483+falgunbokde23@users.noreply.github.com>
2023-01-19 19:40:52 +0530

    Update getAllDepartmentUsecase.java
    update

--- usecases/getAllDepartmentUsecase.java
+++ usecases/getAllDepartmentUsecase.java
@@ -10,31 +10,6 @@ import com.masai.dao.DepartmentDaoImpl;
 public class getAllDepartmentUsecase {
 	
 	
-//	public static void main(String[] args) {
-//		
-//
-//		DepartmentDao dao = new DepartmentDaoImpl();
-//		
-//		try {
-//			
-//		List<Department> dep=dao.allDepartment();
-//		
-//		dep.forEach(e->{
-//			System.out.println("Employee Id is : "+ e.getDname());
-//			System.out.println("Employe name is : "+e.getDNO());
-//			
-//			
-//			System.out.println("=====================================");
-//		});
-//		
-//		
-//		
-//		} catch (departmentException e) {
-//		
-//			System.out.println(e.getMessage());
-//		}
-//	}
-	
 	public static void viewAllDepartment() {
 		DepartmentDao dao = new DepartmentDaoImpl();
 		
