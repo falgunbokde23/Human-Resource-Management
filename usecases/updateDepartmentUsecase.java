782cc23c990c682c8fceb8d32a51e072721a302c
Falgun Bokde <101393483+falgunbokde23@users.noreply.github.com>
2023-01-21 11:20:06 +0530

    Update updateDepartmentUsecase.java
    deleted extra code

--- usecases/updateDepartmentUsecase.java
+++ usecases/updateDepartmentUsecase.java
@@ -8,28 +8,7 @@ import com.masai.dao.DepartmentDaoImpl;
 
 public class updateDepartmentUsecase {
 	
-	public static void main(String[] args) {
-//		
-//		Scanner sc = new Scanner(System.in);
-//		
-//		System.out.println("Enter Department Name : ");
-//		String dname=sc.next();
-//		
-//		System.out.println("Enter Department DId : ");
-//		int did=sc.nextInt();
-//		
-//		DepartmentDao dao=new DepartmentDaoImpl();
-//		
-//		Department department = new Department();
-//		
-//		department.setDname(dname);
-//		department.setDNO(did);
-//		
-//		String result = dao.updateDepartment(department);
-//		
-//		System.out.println(result);
-	}
-	
+
 	
 	public static void updatedept() {
 		
