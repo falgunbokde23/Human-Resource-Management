b93a6e6f0e44a24d5f94a2b2ecf44914fc46c62e
Falgun Bokde <101393483+falgunbokde23@users.noreply.github.com>
2023-01-23 20:28:38 +0530

    Update EmployeeException.java
    update

--- Exception/EmployeeException.java
+++ Exception/EmployeeException.java
@@ -3,12 +3,10 @@ package com.masai.Exception;
 public class EmployeeException extends Exception {
 	
 	public EmployeeException() {
-		
 	}
 	
 	public EmployeeException(String message) {
 		super();
-	
 	}
 
 }
